ca4070ee81fafe8d82680fd31a7ec46a129c0a45
BBH PowPow <86885041+bbhpowpow@users.noreply.github.com>
2021-09-14 22:48:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,84 +13,91 @@
 </head>
 
 <body>
-  <canvas id="canvas"></canvas>
-  <section class="section-top">
-    <nav class="row">
-      <div class="nav">
-        <a class="nav__logo" href="#">MyName</a>
-        <div class="nav__menu-wrapper">
-          <button class="nav__menu"><i class='bx bx-menu-alt-right'></i></button>
-          <div class="nav__menu-dropdown">
-            <div class="nav__menu-links">
-              <a href="form.php" target="_blank" rel="noopener noreferrer" class="nav__menu-link">
-                <i class='bx bx-chevron-right'></i>
-                <span>Submit Suggestions</span>
-              </a>
-              <a href="https://github.com/bbhpowpow/myname" target="_blank" rel="noopener noreferrer" class="nav__menu-link">
-                <i class='bx bx-chevron-right'></i>
-                <span>Github Repository</span>
-              </a>
-            </div>
-            <hr>
-            <div class="nav__menu-links">
-              <a href="https://www.instagram.com/envil.a/" target="_blank" rel="noopener noreferrer" class="nav__menu-link">
-                <i class='bx bx-chevron-right'></i>
-                <span>My Instagram</span>
-              </a>
-              <a href="https://github.com/bbhpowpow" target="_blank" rel="noopener noreferrer" class="nav__menu-link">
-                <i class='bx bx-chevron-right'></i>
-                <span>My Github</span>
-              </a>
-              <a href="https://saweria.co/abinf" target="_blank" rel="noopener noreferrer" class="nav__menu-link">
-                <i class='bx bx-chevron-right'></i>
-                <span>Saweria</span>
-              </a>
+  <div id="container">
+    <canvas id="canvas"></canvas>
+    <section class="section-top">
+      <nav class="row">
+        <div class="nav">
+          <a class="nav__logo" href="#">MyName</a>
+          <div class="nav__menu-wrapper">
+            <button class="nav__menu"><i class='bx bx-menu-alt-right'></i></button>
+            <div class="nav__menu-dropdown">
+              <div class="nav__menu-links">
+                <a href="form.php" target="_blank" rel="noopener noreferrer" class="nav__menu-link">
+                  <i class='bx bx-chevron-right'></i>
+                  <span>Submit Suggestions</span>
+                </a>
+                <a href="https://github.com/bbhpowpow/myname" target="_blank" rel="noopener noreferrer" class="nav__menu-link">
+                  <i class='bx bx-chevron-right'></i>
+                  <span>Github Repository</span>
+                </a>
+              </div>
+              <hr>
+              <div class="nav__menu-links">
+                <a href="https://www.instagram.com/envil.a/" target="_blank" rel="noopener noreferrer" class="nav__menu-link">
+                  <i class='bx bx-chevron-right'></i>
+                  <span>My Instagram</span>
+                </a>
+                <a href="https://github.com/bbhpowpow" target="_blank" rel="noopener noreferrer" class="nav__menu-link">
+                  <i class='bx bx-chevron-right'></i>
+                  <span>My Github</span>
+                </a>
+                <a href="https://saweria.co/abinf" target="_blank" rel="noopener noreferrer" class="nav__menu-link">
+                  <i class='bx bx-chevron-right'></i>
+                  <span>Saweria</span>
+                </a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </nav>
-    <main class="row">
-      <div class="main">
-        <div class="main__header">
-          <h1 class="main__header-text">WELCOME TO OUR WEBSITE</h1>
+      </nav>
+      <main class="row">
+        <div class="main">
+          <div class="main__header">
+            <h1 class="main__header-text">WELCOME TO OUR WEBSITE</h1>
+          </div>
+          <p class="main__text">
+            Dapatkan nama terbaik untuk karaktermu dengan mudah<br>bersama <span class="main__logo">MyName</span>.
+          </p>
+          <div id="btn-wrapper">
+            <a class="btn btn-black" href="#generate">Mulai</a>
+          </div>
         </div>
-        <p class="main__text">
-          Dapatkan nama terbaik untuk karaktermu dengan mudah<br>bersama <span class="main__logo">MyName</span>.
-        </p>
-        <div id="btn-wrapper">
-          <a class="btn btn-black" href="#generate">Mulai</a>
+      </main>
+    </section>
+    <section class="section-main">
+      <div id="particles-js"></div>
+      <div class="lds-ring">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div class="generate" id="generate">
+        <div class="generate__header">
+          <h2 class="generate__header-text">Generate Nama</h2>
         </div>
-      </div>
-    </main>
-  </section>
-  <section class="section-main">
-    <div id="particles-js"></div>
-    <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
-    <div class="generate" id="generate">
-      <div class="generate__header">
-        <h2 class="generate__header-text">Generate Nama</h2>
-      </div>
-      <div class="generate__main">
-        <div class="generate__btn-wrapper">
-          <select class="btn select-generate" name="" id="select-country">
-            <?php for ($i = 0; $i < count(allCountries()); $i++) { ?>
-              <option value="<?= allCountries()[$i][1]; ?>" <?= (allCountries()[$i][1] == 'id_ID' ? 'selected' : '') ?>><?= allCountries()[$i][0]; ?></option>
-            <?php } ?>
-          </select>
-          <select class="btn select-generate" name="" id="select-gender">
-            <option value="female">Perempuan</option>
-            <option value="male" selected>Laki-laki</option>
-          </select>
-          <button class="btn btn-generate" id="btn-generate">Generate</button>
+        <div class="generate__main">
+          <div class="generate__btn-wrapper">
+            <select class="btn select-generate" name="" id="select-country">
+              <?php for ($i = 0; $i < count(allCountries()); $i++) { ?>
+                <option value="<?= allCountries()[$i][1]; ?>" <?= (allCountries()[$i][1] == 'id_ID' ? 'selected' : '') ?>><?= allCountries()[$i][0]; ?></option>
+              <?php } ?>
+            </select>
+            <select class="btn select-generate" name="" id="select-gender">
+              <option value="female">Perempuan</option>
+              <option value="male" selected>Laki-laki</option>
+            </select>
+            <button class="btn btn-generate" id="btn-generate">Generate</button>
+          </div>
+          <h1 class="generate__name">Abi Noval Fauzi</h1>
+          <input class="generate__input" type="text" value="Abi Noval Fauzi" id="input-copy" readonly>
         </div>
-        <h1 class="generate__name">Abi Noval Fauzi</h1>
-        <input style="position: absolute; opacity: 0; pointer-events: none;" type="text" value="Abi Noval Fauzi" id="input-copy">
+        <button class="btn btn-generate btn-salin">Salin</button>
       </div>
-      <button class="btn btn-generate btn-salin">Salin</button>
-    </div>
-    <p id="alert">Tunggu Sebentar</p>
-  </section>
+      <p id="alert">Tunggu Sebentar</p>
+    </section>
+  </div>
   <script src="https://unpkg.com/starback@1.0.0/dist/starback.js"></script>
   <script src="assets/js/particles.min.js"></script>
   <script src="assets/js/app.js"></script>
